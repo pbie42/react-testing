0656e0ce4699b590e49e7d39f2926657f987174a
Paul Bie <pbie@student.42.fr>
2018-06-20 17:52:20 +0200

    [Add] - actions types reducers Root component to set provider w/ store

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from 'actions'
 
 class CommentBox extends Component {
 	state = {
@@ -11,8 +13,7 @@ class CommentBox extends Component {
 	handleSubmit = e => {
 		e.preventDefault()
 
-		// TODO - Call an action creator
-		// And save the comment
+		this.props.saveComment(this.state.comment)
 
 		this.setState({ comment: '' })
 	}
@@ -37,4 +38,7 @@ class CommentBox extends Component {
 	}
 }
 
-export default CommentBox
+export default connect(
+	null,
+	actions
+)(CommentBox)
--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,5 +1,43 @@
 import React from 'react'
-
+import { mount } from 'enzyme'
+import Root from 'Root'
 import CommentBox from 'components/CommentBox'
 
-xit('has a text area and a button', () => {})
+let wrapped
+
+beforeEach(() => {
+	wrapped = mount(
+		<Root>
+			<CommentBox />
+		</Root>
+	)
+})
+
+afterEach(() => {
+	wrapped.unmount()
+})
+
+it('has a text area and a button', () => {
+	expect(wrapped.find('textarea').length).toEqual(1)
+	expect(wrapped.find('button').length).toEqual(1)
+})
+
+describe('text area', () => {
+	beforeEach(() => {
+		wrapped.find('textarea').simulate('change', {
+			target: { value: 'new comment' }
+		})
+		wrapped.update()
+	})
+
+	it('has a text area that users can type in', () => {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+	})
+
+	it('clears text area when the comment is submitted', () => {
+		wrapped.find('form').simulate('submit')
+		wrapped.update()
+
+		expect(wrapped.find('textarea').prop('value')).toEqual('')
+	})
+})
