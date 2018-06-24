bb197b4d1336059c1e67d2cb461c582481d8ce45
Paul Bie <pbie@student.42.fr>
2018-06-24 13:36:50 -0700

    [Add + Test] - initialState prop to Root. Test for li per comment

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Root from 'Root'
+import CommentList from 'components/CommentList'
+
+let wrapped
+
+beforeEach(() => {
+	const initialState = {
+		comments: ['Comment 1', 'Comment 2']
+	}
+	wrapped = mount(
+		<Root initialState={initialState}>
+			<CommentList />
+		</Root>
+	)
+})
+
+it('creates one LI per comment', () => {
+	expect(wrapped.find('li').length).toEqual(2)
+})
