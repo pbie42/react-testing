befcfbcf15de289d31e4ef31af1754dc4c0f63b5
Paul Bie <pbie@student.42.fr>
2018-07-06 15:17:36 -0700

    [Add] - My own async and stateValidator middleware

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
-import reduxPromise from 'redux-promise'
+import async from 'middlewares/async'
+import stateValidator from 'middlewares/stateValidator'
 import reducers from 'reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const enhancer = composeEnhancers(
-	applyMiddleware(reduxPromise)
+	applyMiddleware(async, stateValidator)
 	// other store enhancers if any
 )
 
